90cd4fe867d13e71d4144f636e8fbd04bd0c9272
unknown <jony.bhm@gmail.com>
2025-04-21 19:25:28 -0300

    first push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyBnuut5FMitMBMldE6N7qcrM2SVYoutd9I",
+  
+    authDomain: "idiomas-educacion.firebaseapp.com",
+  
+    projectId: "idiomas-educacion",
+  
+    storageBucket: "idiomas-educacion.firebasestorage.app",
+  
+    messagingSenderId: "391579922321",
+  
+    appId: "1:391579922321:web:75478685c15c39648bb430"
+  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyBnuut5FMitMBMldE6N7qcrM2SVYoutd9I",
+  
+    authDomain: "idiomas-educacion.firebaseapp.com",
+  
+    projectId: "idiomas-educacion",
+  
+    storageBucket: "idiomas-educacion.firebasestorage.app",
+  
+    messagingSenderId: "391579922321",
+  
+    appId: "1:391579922321:web:75478685c15c39648bb430"
+  
+  }
 };
 
 /*
